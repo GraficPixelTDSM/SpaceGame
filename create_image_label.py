fcc01d18396ec9aee32a17e54328b5ad7e8e6435
GraficPixel <thschwarz725@gmail.com>
2024-09-26 16:52:24 +0200

    add text label to image label py to show all the options

--- create_image_label.py
+++ create_image_label.py
@@ -6,6 +6,11 @@ root.minsize(200, 200)  # width, height
 root.geometry("450x350+50+50")
 
 # Create Label in our window
+text = Label(root, text="Nothing will work unless you do.")
+text.pack()
+text2 = Label(root, text="- Maya Angelou")
+text2.pack()
+
 image = PhotoImage(file="meme_1.png")
 img = Label(root, image=image)
 img.pack()
