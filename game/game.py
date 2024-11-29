013c313c16db5b82bfb99678f081774814219c9d
GraficPixel <thschwarz725@gmail.com>
2024-11-29 10:54:31 +0100

    adjust previously changed vars in functions

--- game/game.py
+++ game/game.py
@@ -17,14 +17,14 @@ def generate_field(x, y):
 def set_mines(num):
     check = 0
     for i in range(num):
-        a = randint(0, width - 1)
-        b = randint(0, height - 1)
+        a = randint(0, WIDTH10 - 1)
+        b = randint(0, HEIGHT9 - 1)
         while field[b][a][0] is True:
-            a = randint(0, width - 1)
-            b = randint(0, height - 1)
+            a = randint(0, WIDTH10 - 1)
+            b = randint(0, HEIGHT9 - 1)
         print(a, b)
         field[b][a][0] = True
-        print(field)
+    print(field)
     for j in range(10):
         check = check + int(field[j].count([True, 0]))
     print(check)
